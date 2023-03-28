6ff19632491e25f2ba71088106e9237df7dcf531
leandro <romeroleandro0711@gmail.com>
2023-03-28 19:43:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 100) + 1;
+
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+
+let numbers = [5, 10, 20, 15, 8];
+let max_value = Math.max(...numbers)
+let max = 20
